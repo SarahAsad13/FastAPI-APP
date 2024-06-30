05be69ccd334feedf5ee62cd7cf21396123ce50a
Subtletechie <79634186+SarahAsad13@users.noreply.github.com>
2024-06-30 22:39:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+# Create a FastAPI application
+app = FastAPI()
+
+# Define a route at the root web address ("/")
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Alex"}
